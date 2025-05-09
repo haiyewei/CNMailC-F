554a3f12940f6fcdaa2e16130fa40cecb1178dcd
haiyewei <79199415+haiyewei@users.noreply.github.com>
2025-05-10 04:27:54 +0800

    update

--- lib/pages/mail_settings_page.dart
+++ lib/pages/mail_settings_page.dart
@@ -5,8 +5,47 @@ class MailSettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('邮箱设置页面创建成功'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('邮箱设置'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Card(
+              child: ListTile(
+                leading: const Icon(Icons.add_circle_outline),
+                title: const Text('添加邮箱'),
+                onTap: () {
+                  // 导航到添加邮箱页面
+                },
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            Card(
+              child: ListTile(
+                leading: const Icon(Icons.save_alt),
+                title: const Text('已保存的邮箱'),
+                onTap: () {
+                  // 导航到已保存的邮箱页面
+                },
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            Card(
+              child: ListTile(
+                leading: const Icon(Icons.edit),
+                title: const Text('编辑邮箱'),
+                onTap: () {
+                  // 导航到编辑邮箱页面
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
